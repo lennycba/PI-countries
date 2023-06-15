7c2b24067cb2dc624977898c0446ac0a856372af
mario lujan <mario_lujan88@hotmail.com>
2023-06-15 00:14:09 -0300

    primeros avances en estilos, pendiente manejo de errores y paginado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,13 +6,14 @@ const Card = ({name,flag,continent,capital,ID})=>{
     return(
        
         <div className={style.card}>
-            <Link to={`${ROUTES.DETAIL}/${ID}`}>
+            <Link to={`${ROUTES.DETAIL}/${ID}`} className={style.link}>
             <img className={style.img} src={flag}/>
             <div className={style.name}>
                 <h2>{name}</h2>
             </div>
-            <h3>{capital}</h3>
-            <h3>{continent}</h3>
+            <div className={style.continent}>
+                <h3>{continent}</h3>
+            </div>
             </Link>
         </div>
 
--- client/src/components/DetailCard/DetailCard.jsx
+++ client/src/components/DetailCard/DetailCard.jsx
@@ -1,28 +1,49 @@
-import style from './DetailCard.module.css'
+import style from "./DetailCard.module.css";
+import checkIcon from '../../images/iconos/check.svg'
 
-const DetailCard = ({name,flag,continent,capital,activities,id,area,subRegion,population})=>{
-
-    return(
-       
-        <div className={style.card}>
-            <img className={style.img} src={flag}/>
-            <div className={style.name}>
-            <h2>{name}</h2>
+const DetailCard = ({
+  name,
+  flag,
+  continent,
+  capital,
+  activities,
+  id,
+  area,
+  subRegion,
+  population,
+}) => {
+  return (
+    <div className={style.card}>
+      <img className={style.img} src={flag} />
+            <div className={style.countId}>
+            <h3>Id: {id}</h3>
             </div>
-            <h3>{id}</h3>
-            <h3>{capital}</h3>
-            <h3>{continent}</h3>
-            <h3>{subRegion}</h3>
-            <h3>{area}</h3>
-            <h3>{population}</h3>
-            <div>{activities?.flat().map((act,index) => <h4 key={index} >{act}</h4>)}</div>
-
+        <div className={style.countName}>
+            <h2>Name: {name}</h2>
+        </div>
+        <div className={style.specs}>
+            <h3>Continent: {continent}</h3>
+            <h3>Sub Region: {subRegion}</h3>
+            <h3>Capital: {capital}</h3>
+            <h3>Population: {population}</h3>
+            <h3>Area {area}m2</h3>
+        </div>
+        <div className={style.act}>
+         <h3>Activities: </h3> 
+          {activities?.flat().map((act, index) => (
+            <div className={style.actItem}>
+                <div className={style.check}>
+                    <img src={checkIcon} />
+                </div> 
+                <div className={style.actName}>
+                    <h4 key={index}> {act}</h4>
+                </div>
+            </div>
+          ))}
         </div>
 
-        
-    )
-}
-
-
+    </div>
+  );
+};
 
 export default DetailCard;
